eaf0b04991a6f8fa2c94d72a595491f220f2088e
Firdaus Bina <142188072+Biancawulan@users.noreply.github.com>
2024-01-15 11:34:21 +0800

    Add files via upload

---
+++ negara/tuvalu.js
@@ -0,0 +1,17 @@
+let  negara = "Tuvalu";
+let ibukota = "funafuti";
+const benua = "osenia";
+let populasi = 11204 ;
+const kepulauan = true;
+let bahasa = "inggris";
+
+function show() {
+    console.log(`
+    Nama Negara : ${negara}
+    Ibukota : ${ibukota}
+    Benua : ${benua}
+    Populasi ; ${populasi}
+    Kepulauan : ${kepulauan}
+    Bahasa : ${bahasa}
+    `);
+}
