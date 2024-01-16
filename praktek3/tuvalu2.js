504a90f3a2c2629106d263be4d5833635cc3457d
Firdaus Bina <142188072+Biancawulan@users.noreply.github.com>
2024-01-16 15:24:47 +0800

    Add files via upload

---
+++ praktek3/tuvalu2.js
@@ -0,0 +1,10 @@
+let  negara = "Tuvalu";
+let ibukota = "funafuti";
+const benua = "osenia";
+let populasi = 11204 ;
+const kepulauan = true;
+let bahasa = "inggris";
+
+    console.log(`
+    Negara ${negara} adalah negara yang berada di benua ${benua}, dengan populasi ${populasi} jiwa, ibukota dari negara ${negara} sendiri adalah kota ${ibukota}, bahasa resmi yang digunakan adalah bahasa ${bahasa}
+    `);
