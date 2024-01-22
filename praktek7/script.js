1443108a4ffd934722f077eef3f148c5890c3a3c
Firdaus Bina <142188072+Biancawulan@users.noreply.github.com>
2024-01-22 13:55:23 +0800

    Add files via upload

---
+++ praktek7/script.js
@@ -0,0 +1,52 @@
+// let silaPancasila
+
+// switch (silaPancasila) {
+//     case 1:
+//         document.write(`Ketuhanan Yang Maha Esa`)
+//     break;
+
+//     case 2:
+//         document.write(`Kemanusiaan Yang Adil Dan Beraadab`)
+//     break;
+
+//     case 3:
+//         document.write(`Persatuan Indonesia`)
+//     break;
+
+//     case 4:
+//         document.write(`Kerakyatan Yang Dipimpin Oleh Hikmat Kebijaksanaan Dalam Permusyawaratan Perwakilan`)
+//     break;
+
+//     case 5:
+//         document.write(`Keadilan Sosial Bagi seluruh Rakyat Indonesia`)
+//     break;
+//     default:
+//         document.write(`Nilai Tidak Valid`);
+// }
+
+let kode_Plat
+
+switch (kode_Plat) {
+    case "ku":
+        document.write( `KU Adalah kode Nomor polisi Dari Provinsi Kalimantan Utara`)
+    break;
+
+    case "kt":
+        document.write(`KT Adalah Kode Nomor Polisi Dari Provinsi Kalimantan Timur`)
+    break;
+
+    case"kb":
+        document.write(`KB Adalah Kode Nomor Polisi Dari Provinsi Kalimantan Barat`)
+    break;
+
+    case "kh";
+        document.write(`KH Adalah Kode Nomor Polisi Dari Provisi Kalimantan Tengah`)
+
+    case "da":
+        document.write(`DA Adalah Nomor Polisi Dari Provinsi Kalimantan Selatan`)
+    break;
+
+    default:
+        document.write(`Kode yang Anda Masuki Masih Belum Terdaftar`)
+    break;
+}
