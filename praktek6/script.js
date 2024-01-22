27b7f75b2533bb09cdab258721866fc5d31066ba
Firdaus Bina <142188072+Biancawulan@users.noreply.github.com>
2024-01-22 13:56:12 +0800

    Add files via upload

---
+++ praktek6/script.js
@@ -0,0 +1,29 @@
+//contoh 1
+// let number = 10 + "10"
+
+// console.log(number);
+
+//Contoh 2
+
+// let str = "42"
+// let num = ++str;
+
+// console.log(num);
+
+// const birthYear = prompt("Tahun Berapa kamu lahir ?")
+// const yourAge = 2024 - Number(birthYear);
+// console.log(yourAge);
+
+// let num = 42;
+// let str = String(num);
+// console.log(str);
+
+const age = 20;
+const height = 167;
+const weight = 60;
+const isStudenr = true;
+
+const ageAsString = String(age);
+const heightAsString = height + "cm";
+const weightAsString = weight + "kg";
+const StudendStatus = isStudenr(Number);
